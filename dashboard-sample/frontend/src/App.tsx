91773384e62d659061044cded797e17f34a9d18b
t-akzw <45358756+t-akzw@users.noreply.github.com>
2022-05-22 23:50:57 +0900

    init dashboard

---
+++ dashboard-sample/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/no-unstable-nested-components */
+/* eslint-disable react/function-component-definition */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable no-shadow */
+/* eslint-disable prefer-arrow/prefer-arrow-functions */
+import React from 'react';
+import './App.css';
+import DashboardContent from './dashboard/Dashboard';
+
+const App = () => <DashboardContent />;
+
+export default App;
